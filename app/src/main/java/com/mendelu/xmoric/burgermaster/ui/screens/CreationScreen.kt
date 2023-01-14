416f6c154273eaa9886915a507ac677f3ef53bc2
MarekMoric <xmoric@mendelu.cz>
2023-01-14 22:45:18 +0100

    customizing creation screen, half done

--- app/src/main/java/com/mendelu/xmoric/burgermaster/ui/screens/CreationScreen.kt
+++ app/src/main/java/com/mendelu/xmoric/burgermaster/ui/screens/CreationScreen.kt
@@ -1,24 +1,18 @@
 package com.mendelu.xmoric.burgermaster.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import android.graphics.drawable.Icon
+import android.graphics.drawable.shapes.Shape
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.toSize
+import coil.decode.ImageSource
+import com.mendelu.xmoric.burgermaster.R
 import com.mendelu.xmoric.burgermaster.navigation.INavigationRouter
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,45 +33,96 @@ fun CreationScreen(navigation: INavigationRouter) {
 //            fontSize = 20.sp
 //        )
 
-        var mExpanded by remember { mutableStateOf(false) }
-        val mBreadTypes = listOf("White", "Dark", "Rustical")
-        var mSelectedText by remember { mutableStateOf("") }
-        var mTextFieldSize by remember { mutableStateOf(0)}
+    val breadTypes = listOf("White", "Dark", "Rustical")
+    val meatTypes = listOf("1/2", "1x", "2x", "3x")
+    val sauceTypes = listOf("Ketchup", "Mustard", "Bacon")
+    val extrasTypes = listOf("Lettuce", "Bacon", "Tomato", "Pickles")
+    var description by rememberSaveable { mutableStateOf("") }
 
-        val icon = if (mExpanded)
-            Icons.Filled.KeyboardArrowUp
-        else
-            Icons.Filled.KeyboardArrowDown
 
-        Column(Modifier.padding(20.dp)) {
+    Column {
+        DropdownList(content = breadTypes, header = "Bread type", false)
+        DropdownList(content = meatTypes, header = "Meat", false)
+        DropdownList(content = sauceTypes, header = "Sauce", true)
+        DropdownList(content = extrasTypes, header = "Extras", true)
+        OutlinedTextField(
+            value = description,
+            onValueChange = { description = it},
+            label = { Text(text = "Do you have any specials needs?")},
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.Black,
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(start = 8.dp, end = 8.dp)
+        )
+    }
+}
 
-            // Create an Outlined Text Field
-            // with icon and not expanded
-            OutlinedTextField(
-                value = mSelectedText,
-                onValueChange = { mSelectedText = it },
-                modifier = Modifier
-                    .fillMaxWidth(),
-                label = {Text("Bread Type")},
-                trailingIcon = {
-                    Icon(icon,"contentDescription",
-                        Modifier.clickable { mExpanded = !mExpanded })
-                }
-            )
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownList(content: List<String>, header: String, checkboxes: Boolean){
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(content[1]) }
+    var checked: Boolean = false
+    var icon: Int = 0
 
-            // Create a drop-down menu with list of cities,
-            // when clicked, set the Text Field text as the city selected
-            DropdownMenu(
-                expanded = mExpanded,
-                onDismissRequest = { mExpanded = false },
-                modifier = Modifier.width(with(LocalDensity.current){mTextFieldSize.toDp()})
-            ) {
-                mBreadTypes.forEach { label ->
-                    DropdownMenuItem(onClick = {
-                        mSelectedText = label
-                        mExpanded = false
-                    }, text = { Text(text = label) })
-                }
+    when (header) {
+        "Bread type" -> icon = R.drawable.burger_meat
+        "Meat" -> icon =  R.drawable.burger_meat
+        "Sauce" -> icon =  R.drawable.sauce
+        "Extras" -> icon =  R.drawable.bacon
+        else -> {
+            icon =  R.drawable.bacon
+        }
+    }
+
+    ExposedDropdownMenuBox(
+        modifier = Modifier.padding(16.dp),
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+    ) {
+        TextField(
+            readOnly = true,
+            value = selectedOptionText,
+            onValueChange = {},
+            label = { Text(header) },
+            leadingIcon = { Image(painterResource(
+                id = icon),
+                contentDescription = "",
+                modifier = Modifier.size(48.dp)) },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.Black,
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(),
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            content.forEach { item ->
+                DropdownMenuItem(
+                    text = { Text(item) },
+                    onClick = {
+                        selectedOptionText = item
+                        expanded = false
+                    },
+                    trailingIcon = {
+                        if (checkboxes) {
+//                        Checkbox(checked = checked, onCheckedChange = { selectedOptionText += ", $item"; checked = true }) }
+                        Text("Add Extra", style = MaterialTheme.typography.bodySmall) } //vymen za button, mas tam totizto onlick ktory potrebujes
+                     }
+                )
             }
         }
     }
+}
+
+
+
